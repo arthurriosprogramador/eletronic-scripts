d64493d610c384485e0ddb045e53317ae150e6fc
Arthur Rios Ribeiro <arthurriosribeiro@gmail.com>
2026-09-06 22:50:00 -0300

    Initiate weather station script

---
+++ weather_station/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  Wire.begin(21, 22);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("Falha ao inicializar o OLED. Verifique o endereço ou conexões."));
+    for(;;);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 20);
+  display.println("HELLO WORLD!");
+  display.display();
+}
+
+void loop() {
+
+}
